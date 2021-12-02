fc370bf3152b4a759e80b0476664d17abe9dc41c
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2021-12-01 21:47:14 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,13 @@ import { ChakraProvider } from "@chakra-ui/react"
 import theme from "../lib/chakra-theme"
 import GlobalStyles from "../styles/GlobalStyles"
 import Main from "../components/main"
+import Fonts from "../components/fonts"
+
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <AnimatePresence exitBeforeEnter initial={true}>
         <Main>
           <GlobalStyles />
